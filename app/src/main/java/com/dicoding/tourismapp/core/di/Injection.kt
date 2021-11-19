f3a9be7e5d5752f9dce44d8dad26234984ca5ca1
amrilhakimsihotang <amrilhakimsihotang@outlook.com>
2021-11-19 21:39:48 +0700

    Remove JSon and replace with APIConfig

--- app/src/main/java/com/dicoding/tourismapp/core/di/Injection.kt
+++ app/src/main/java/com/dicoding/tourismapp/core/di/Injection.kt
@@ -5,6 +5,8 @@ import com.dicoding.tourismapp.core.data.TourismRepository
 import com.dicoding.tourismapp.core.data.source.local.LocalDataSource
 import com.dicoding.tourismapp.core.data.source.local.room.TourismDatabase
 import com.dicoding.tourismapp.core.data.source.remote.RemoteDataSource
+import com.dicoding.tourismapp.core.data.source.remote.network.ApiConfig
+import com.dicoding.tourismapp.core.data.source.remote.network.ApiService
 import com.dicoding.tourismapp.core.utils.AppExecutors
 import com.dicoding.tourismapp.core.utils.JsonHelper
 
@@ -12,7 +14,7 @@ object Injection {
     fun provideRepository(context: Context): TourismRepository {
         val database = TourismDatabase.getInstance(context)
 
-        val remoteDataSource = RemoteDataSource.getInstance(JsonHelper(context))
+        val remoteDataSource = RemoteDataSource.getInstance(ApiConfig.provideApiService())
         val localDataSource = LocalDataSource.getInstance(database.tourismDao())
         val appExecutors = AppExecutors()
 
